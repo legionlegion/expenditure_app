9d2613af114709d3a6e5223fe5f4380c7a7700ad
Shuyuan <crayy.0803@gmail.com>
2022-05-16 23:29:09 +0800

    ListView usage

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,55 +10,103 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: SingleChildScrollView(
-        child: Column(
-          children: transactions.map((tx) {
-            return Card(
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.all(10),
-                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.purple,
-                        width: 2,
-                      ),
-                    ),
-                    child: Text(
-                      '\$${tx.amount}',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.purple,
-                      ),
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          return Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(10),
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
                     ),
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        tx.title,
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
+                  child: Text(
+                    '\$${transactions[index].amount}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple,
+                    ),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      transactions[index].title,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
                       ),
-                      Text(
-                        DateFormat('yyyy/MM/dd').format(tx.date),
-                        style: TextStyle(
-                          fontSize: 10,
-                          color: Colors.grey,
-                        ),
+                    ),
+                    Text(
+                      DateFormat('yyyy/MM/dd').format(transactions[index].date),
+                      style: TextStyle(
+                        fontSize: 10,
+                        color: Colors.grey,
                       ),
-                    ],
-                  )
-                ],
-              ),
-            );
-          }).toList(),
-        ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          );
+        },
+        itemCount: transactions.length,
       ),
+      // * Less elegant method without ListView.builder
+      // * ListView = Column + SingleChildScrollView
+      // child: ListView(
+      //   children: transactions.map((tx) {
+      //     return Card(
+      //       child: Row(
+      //         children: <Widget>[
+      //           Container(
+      //             padding: EdgeInsets.all(10),
+      //             margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+      //             decoration: BoxDecoration(
+      //               border: Border.all(
+      //                 color: Colors.purple,
+      //                 width: 2,
+      //               ),
+      //             ),
+      //             child: Text(
+      //               '\$${tx.amount}',
+      //               style: TextStyle(
+      //                 fontWeight: FontWeight.bold,
+      //                 fontSize: 20,
+      //                 color: Colors.purple,
+      //               ),
+      //             ),
+      //           ),
+      //           Column(
+      //             crossAxisAlignment: CrossAxisAlignment.start,
+      //             children: <Widget>[
+      //               Text(
+      //                 tx.title,
+      //                 style: TextStyle(
+      //                   fontSize: 16,
+      //                   fontWeight: FontWeight.bold,
+      //                 ),
+      //               ),
+      //               Text(
+      //                 DateFormat('yyyy/MM/dd').format(tx.date),
+      //                 style: TextStyle(
+      //                   fontSize: 10,
+      //                   color: Colors.grey,
+      //                 ),
+      //               ),
+      //             ],
+      //           )
+      //         ],
+      //       ),
+      //     );
+      //   }).toList(),
+      // ),
     );
   }
 }
